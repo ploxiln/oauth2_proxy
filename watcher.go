937d2bbba7939a01aeb2489d7866093707aad80a
Pierce Lopez <pierce.lopez@gmail.com>
2018-11-26 04:02:58 -0500

    watcher: minor refactor
    move goroutine loop into named function,
    to make debugging backtraces a bit easier
    
    make debugging backtraces a bit easier
    since the watcher tests are still a bit flaky on travis-ci
    
    extend replacement wait time from 2 seconds to 5 seconds -
    originally it looped waiting indefinitely, so make it a bit
    more forgiving, hopefully it won't be a big problem for many users ;)

--- watcher.go
+++ watcher.go
@@ -12,7 +12,7 @@ import (
 )
 
 func WaitForReplacement(filename string, watcher *fsnotify.Watcher) {
-	for i := 0; i < 20; i++ {
+	for i := 0; i < 50; i++ {
 		time.Sleep(100 * time.Millisecond)
 
 		if _, err := os.Stat(filename); err == nil {
@@ -25,39 +25,41 @@ func WaitForReplacement(filename string, watcher *fsnotify.Watcher) {
 	log.Printf("failed to resume watching for %s", filename)
 }
 
+func watchLoop(filename string, watcher *fsnotify.Watcher, done <-chan bool, action func()) {
+	for {
+		select {
+		case _ = <-done:
+			log.Printf("Shutting down watcher for: %s", filename)
+			watcher.Close()
+			return
+		case event := <-watcher.Events:
+			// On Arch Linux, it appears Chmod events precede Remove events,
+			// which causes a race between action() and the coming Remove event.
+			if event.Op == fsnotify.Chmod {
+				continue
+			}
+			if event.Op&(fsnotify.Remove|fsnotify.Rename) != 0 {
+				log.Printf("watching interrupted on event: %s", event)
+				watcher.Remove(filename)
+				WaitForReplacement(filename, watcher)
+			}
+			log.Printf("reloading after event: %s", event)
+			action()
+		case err := <-watcher.Errors:
+			log.Printf("error watching %s: %s", filename, err)
+		}
+	}
+}
+
 func WatchForUpdates(filename string, done <-chan bool, action func()) {
 	filename = filepath.Clean(filename)
 	watcher, err := fsnotify.NewWatcher()
 	if err != nil {
 		log.Fatal("failed to create watcher for ", filename, ": ", err)
 	}
-	go func() {
-		for {
-			select {
-			case _ = <-done:
-				log.Printf("Shutting down watcher for: %s", filename)
-				watcher.Close()
-				return
-			case event := <-watcher.Events:
-				// On Arch Linux, it appears Chmod events precede Remove events,
-				// which causes a race between action() and the coming Remove event.
-				if event.Op == fsnotify.Chmod {
-					continue
-				}
-				if event.Op&(fsnotify.Remove|fsnotify.Rename) != 0 {
-					log.Printf("watching interrupted on event: %s", event)
-					watcher.Remove(filename)
-					WaitForReplacement(filename, watcher)
-				}
-				log.Printf("reloading after event: %s", event)
-				action()
-			case err := <-watcher.Errors:
-				log.Printf("error watching %s: %s", filename, err)
-			}
-		}
-	}()
 	if err = watcher.Add(filename); err != nil {
 		log.Fatal("failed to add ", filename, " to watcher: ", err)
 	}
+	go watchLoop(filename, watcher, done, action)
 	log.Printf("watching %s for updates", filename)
 }
